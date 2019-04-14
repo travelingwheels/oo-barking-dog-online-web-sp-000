8209d7edb1d158ce6a48a36080be7e57a89382c6
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-14 15:50:04 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def bark=(bark)
+    @bark = bark
+  end
+  
+  def bark
+    @bark
+    puts "woof!"
+  end
+end
+  
